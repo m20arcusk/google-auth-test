40e82d49f4f6fdc6f4fc673a678c8410b86fb17b
m20arcusk <marcuskam20@gmail.com>
2024-05-30 14:05:12 -0700

    google auth button added and returns token upon sign in

--- oauth-test/src/App.js
+++ oauth-test/src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from 'react';
+import { jwtDecode } from "jwt-decode";
+
 
 function App() {
+
+  function handleCallbackResponse(response) {
+      console.log("Encoded JWT ID token:" + response.credential);
+      var userObject = jwtDecode(response.credential);
+      console.log(userObject);
+  }
+
+  useEffect(() => {
+    /* global google */
+    google.accounts.id.initialize ({
+      client_id: process.env.REACT_APP_CLIENT_ID,
+      callback: handleCallbackResponse,
+    });
+
+    google.accounts.id.renderButton(
+      document.getElementById("signInDiv"),
+      {theme: "outline", size: "large"}
+    )
+
+
+  }, []);
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" >
+      <div id="signInDiv"></div>
     </div>
   );
 }
 
 export default App;
+
